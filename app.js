4b94588988e02dbc79a5c280327a77ebfc52fdce
Ritika Pathak <ritika8949@gmail.com>
2023-10-31 20:48:50 +0530

    Implemented the feature of game reset and score display

--- app.js
+++ app.js
@@ -8,11 +8,11 @@ let level = 0;
 
 let h2 = document.querySelector("h2");
 
-document.addEventListener("keypress", function(){
+document.addEventListener("keypress", function(e){
+    console.dir(e.key);
     if(started == false){
         console.log("game is started");
         started = true;
-
         levelUp();
     }
 });
@@ -32,25 +32,49 @@ function userFlash(btn) {
 }
 
 function levelUp(){
+    userSeq = [];
     level++;
     h2.innerText = `Level ${level}`;
-
     let randIdx = Math.floor(Math.random() * 3);
     let randColor = btns[randIdx];
     let randBtn = document.querySelector(`.${randColor}`);
-    console.log(randIdx);
-    console.log(randColor);
-    console.log(randBtn);
+    gameSeq.push(randColor);
+    console.log(gameSeq);
     gameflash(randBtn);
+}
+
+function checkAns(idx){
+    if( userSeq[idx] === gameSeq[idx]){
+        if(userSeq.length == gameSeq.length){
+            setTimeout(levelUp, 1000);
+        }
+    } else {
+        h2.innerHTML = `Game Over! Your Score was <b>${level}</b> <br> Press any key to start.`;
+        document.querySelector("body").style.backgroundColor = "red";
+        setTimeout(function() {
+            document.querySelector("body").style.backgroundColor = "white";
+        }, 150);
+        reset();
+    }
 }
 
 function btnPress() {
     let btn = this;
-    console.log(this);
     userFlash(btn);
+    userColor = btn.getAttribute("id");
+    userSeq.push(userColor);
+    console.log(userSeq);
+    checkAns(userSeq.length-1);
 }
 
 let allBtns = document.querySelectorAll(".btn");
 for(btn of allBtns){
     btn.addEventListener("click", btnPress);
+}
+
+function reset() {
+    started = false;
+    gameSeq = [];
+    userSeq = [];
+    level = 0;
 }
